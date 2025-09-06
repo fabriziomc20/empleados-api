502750dcff8d411d828a18e6714a62faf5f6c20a
fabriziomc20 <126849645+fabriziomc20@users.noreply.github.com>
2025-09-05 22:57:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,16 @@ const streamifier = require("streamifier");
 
 const app = express();
 
+const allowOrigin = "https://fabriziomc20.github.io";
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", allowOrigin);
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") return res.sendStatus(200); // preflight
+  next();
+});
+
 /* =========================
    CORS (tu frontend)
    ========================= */
